895f11298b9d18282430f444148d5927c6a988e9
Helen <helenyarinovskaya@gmail.com>
2017-01-31 15:50:47 +0300

    doubly-linked-list/feature: new constructor, append, head and tail methods

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,13 +1,40 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this._head = null;
+        this._tail = null;
+        this.length = 0;
+    }
 
-    append(data) {}
+    append(data) {
+        var node = new Node(data);
+        if(!this.length) {
+            this.head(node);
+            this.tail(node);
+        } else {
+            this._tail.next = node;
+            node.prev = this._tail;
+            this.tail(node);
+        }
+        this.length++; 
+    }
 
-    head() {}
+    head() {
+        if(arguments.length) {
+            this._head = arguments[0];
+        } else {
+            return this._head ? this._head.data : null;
+        }
+    }
 
-    tail() {}
+    tail() {
+        if(arguments.length) {
+            this._tail = arguments[0];
+        } else {
+            return this.tail ? this._tail.data : null;
+        }
+    }
 
     at(index) {}
 
